d2a2efc16ed5a2dc7d914be231ee22b7bf574197
Nenad <nbursa@gmail.com>
2022-03-11 16:27:37 +0100

    verification

--- authorization-server.js
+++ authorization-server.js
@@ -1,68 +1,70 @@
-const fs = require("fs")
-const express = require("express")
-const bodyParser = require("body-parser")
-const jwt = require("jsonwebtoken")
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 const {
-	randomString,
-	containsAll,
-	decodeAuthCredentials,
-	timeout,
-} = require("./utils")
+  randomString,
+  containsAll,
+  decodeAuthCredentials,
+  timeout,
+} = require('./utils');
 
 const config = {
-	port: 9001,
-	privateKey: fs.readFileSync("assets/private_key.pem"),
+  port: 9001,
+  privateKey: fs.readFileSync('assets/private_key.pem'),
 
-	clientId: "my-client",
-	clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
-	redirectUri: "http://localhost:9000/callback",
+  clientId: 'my-client',
+  clientSecret: 'zETqHgl0d7ThysUqPnaFuLOmG1E=',
+  redirectUri: 'http://localhost:9000/callback',
 
-	authorizationEndpoint: "http://localhost:9001/authorize",
-}
+  authorizationEndpoint: 'http://localhost:9001/authorize',
+};
 
 const clients = {
-	"my-client": {
-		name: "Sample Client",
-		clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
-		scopes: ["permission:name", "permission:date_of_birth"],
-	},
-	"test-client": {
-		name: "Test Client",
-		clientSecret: "TestSecret",
-		scopes: ["permission:name"],
-	},
-}
+  'my-client': {
+    name: 'Sample Client',
+    clientSecret: 'zETqHgl0d7ThysUqPnaFuLOmG1E=',
+    scopes: ['permission:name', 'permission:date_of_birth'],
+  },
+  'test-client': {
+    name: 'Test Client',
+    clientSecret: 'TestSecret',
+    scopes: ['permission:name'],
+  },
+};
 
 const users = {
-	user1: "password1",
-	john: "appleseed",
-}
+  user1: 'password1',
+  john: 'appleseed',
+};
 
-const requests = {}
-const authorizationCodes = {}
+const requests = {};
+const authorizationCodes = {};
 
-let state = ""
+let state = '';
 
-const app = express()
-app.set("view engine", "ejs")
-app.set("views", "assets/authorization-server")
-app.use(timeout)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'assets/authorization-server');
+app.use(timeout);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /*
 Your code here
 */
 app.get('/authorize', (req, res) => {
-	// console.log(res)
-	res.end(res.status)
+  const client = req.query['client_id'];
+  return Object.keys(clients).includes(client)
+    ? res.end(res.status(200))
+    : res.end(res.status(401));
 });
 
-const server = app.listen(config.port, "localhost", function () {
-	var host = server.address().address
-	var port = server.address().port
-})
+const server = app.listen(config.port, 'localhost', function () {
+  var host = server.address().address;
+  var port = server.address().port;
+});
 
 // for testing purposes
 
-module.exports = { app, requests, authorizationCodes, server }
+module.exports = { app, requests, authorizationCodes, server };
