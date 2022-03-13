d5f7c7484b93445e79c0202d3e2e1d9268f53a4b
Nenad <nbursa@gmail.com>
2022-03-13 20:02:19 +0100

    bug

--- authorization-server.js
+++ authorization-server.js
@@ -1,122 +1,116 @@
-const fs = require('fs');
-const express = require('express');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
+const url = require("url");
+const fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 const {
   randomString,
   containsAll,
   decodeAuthCredentials,
   timeout,
-} = require('./utils');
+} = require("./utils");
 
 const config = {
   port: 9001,
-  privateKey: fs.readFileSync('assets/private_key.pem'),
+  privateKey: fs.readFileSync("assets/private_key.pem"),
 
-  clientId: 'my-client',
-  clientSecret: 'zETqHgl0d7ThysUqPnaFuLOmG1E=',
-  redirectUri: 'http://localhost:9000/callback',
+  clientId: "my-client",
+  clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
+  redirectUri: "http://localhost:9000/callback",
 
-  authorizationEndpoint: 'http://localhost:9001/authorize',
+  authorizationEndpoint: "http://localhost:9001/authorize",
 };
 
 const clients = {
-  'my-client': {
-    name: 'Sample Client',
-    clientSecret: 'zETqHgl0d7ThysUqPnaFuLOmG1E=',
-    scopes: ['permission:name', 'permission:date_of_birth'],
+  "my-client": {
+    name: "Sample Client",
+    clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
+    scopes: ["permission:name", "permission:date_of_birth"],
   },
-  'test-client': {
-    name: 'Test Client',
-    clientSecret: 'TestSecret',
-    scopes: ['permission:name'],
+  "test-client": {
+    name: "Test Client",
+    clientSecret: "TestSecret",
+    scopes: ["permission:name"],
   },
 };
 
 const users = {
-  user1: 'password1',
-  john: 'appleseed',
+  user1: "password1",
+  john: "appleseed",
 };
 
 const requests = {};
 const authorizationCodes = {};
 
-let state = '';
+let state = "";
 
 const app = express();
-app.set('view engine', 'ejs');
-app.set('views', 'assets/authorization-server');
+app.set("view engine", "ejs");
+app.set("views", "assets/authorization-server");
 app.use(timeout);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/*
-Your code here
-*/
-
-// 1
-app.get('/authorize', (req, res) => {
+app.get("/authorize", (req, res) => {
   const clientId = req.query.client_id;
   const client = clients[clientId];
   if (!client) {
-    res.status(401).send('Error: user not authorized!');
+    res.status(401).send("Error: client not authorized");
     return;
   }
   if (
-    typeof req.query.scope !== 'string' ||
-    !containsAll(client.scopes, req.query.scope.split(' '))
+    typeof req.query.scope !== "string" ||
+    !containsAll(client.scopes, req.query.scope.split(" "))
   ) {
-    res.status(401).send('Error: invalid scopes requested!');
+    res.status(401).send("Error: invalid scopes requested");
     return;
   }
   const requestId = randomString();
   requests[requestId] = req.query;
-  res.render('login', {
+  res.render("login", {
     client,
     scope: req.query.scope,
     requestId,
   });
 });
 
-// 2
-app.post('/approve', (req, res) => {
+app.post("/approve", (req, res) => {
   const { userName, password, requestId } = req.body;
   if (!userName || users[userName] !== password) {
-    res.status(401).send('Error: Credentials do not match!');
+    res.status(401).send("Error: user not authorized");
     return;
   }
-  const clientRequest = requests[requestId];
+  const clientReq = requests[requestId];
   delete requests[requestId];
-  if (!clientRequest) {
-    res.status(401).send('Error: Invalid user request!');
+  if (!clientReq) {
+    res.status(401).send("Error: invalid user request");
     return;
   }
   const code = randomString();
-  authorizationCodes[code] = { clientRequest, userName };
-  const redirectUri = url.parse(clientRequest.redirect_uri);
+  authorizationCodes[code] = { clientReq, userName };
+  const redirectUri = url.parse(clientReq.redirect_uri);
   redirectUri.query = {
     code,
-    state: clientRequest.state,
+    state: clientReq.state,
   };
-  res.sendStatus(200).redirect(url.format(redirectUri));
+  res.redirect(url.format(redirectUri));
 });
 
-// 3
-app.post('/token', (req, res) => {
+app.post("/token", (req, res) => {
   let authCredentials = req.headers.authorization;
   if (!authCredentials) {
-    res.status(401).send('Error: Not authorized!');
+    res.status(401).send("Error: not authorized");
     return;
   }
   const { clientId, clientSecret } = decodeAuthCredentials(authCredentials);
   const client = clients[clientId];
   if (!client || client.clientSecret !== clientSecret) {
-    res.status(401).send('Error: Client not authorized!');
+    res.status(401).send("Error: client not authorized");
     return;
   }
   const code = req.body.code;
   if (!code || !authorizationCodes[code]) {
-    res.status(401).send('Error: Invalid code!');
+    res.status(401).send("Error: invalid code");
     return;
   }
   const { clientReq, userName } = authorizationCodes[code];
@@ -128,19 +122,19 @@ app.post('/token', (req, res) => {
     },
     config.privateKey,
     {
-      algorithm: 'RS256',
+      algorithm: "RS256",
       expiresIn: 300,
-      issuer: 'http://localhost' + config.port,
+      issuer: "http://localhost:" + config.port,
     }
   );
   res.json({
     access_token: token,
-    token_type: 'Bearer',
+    token_type: "Bearer",
     scope: clientReq.scope,
   });
 });
 
-const server = app.listen(config.port, 'localhost', function () {
+const server = app.listen(config.port, "localhost", function () {
   var host = server.address().address;
   var port = server.address().port;
 });
